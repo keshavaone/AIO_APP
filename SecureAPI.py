48dfe0111dedaf4cacb13616b394b3212dc16590
Keshava <keshava_one@outlook.com>
2024-09-22 21:34:13 +0530

    Upgraded Logs Collection Policy

--- SecureAPI.py
+++ SecureAPI.py
@@ -8,6 +8,7 @@ import ast, datetime
 import CONSTANTS
 from pymongo import MongoClient # type: ignore
 from pymongo.server_api import ServerApi # type: ignore
+import threading
 
 @dataclass(eq=False, repr=False, order=False)
 class Agent:
@@ -248,9 +249,9 @@ class Agent:
 
     def collect_logs(self):
         s3 = boto3.client('s3')
-        log_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         with open('application.log','rb') as f:
-            s3.upload_fileobj(f, self.s3, f'application_log_{log_date}.log')
+            s3.upload_fileobj(f, self.s3, f'logs/application_{log_date}.log')
             # os.remove('application.log')
             return True
         return False
